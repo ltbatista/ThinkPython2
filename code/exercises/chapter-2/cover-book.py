abdb36968096357b2eb9622ad7d78ccd89775189
ltbatista30 <ltbatista30@gmail.com>
2020-07-02 18:52:07 -0300

    correcao exercicio

--- code/exercises/chapter-2/cover-book.py
+++ code/exercises/chapter-2/cover-book.py
@@ -2,18 +2,19 @@ def calc_price(cb_price, copies):
     ## calculates the 40% off
     price = cb_price*0.6
     ## calculates the shipping cost
-    price = price + 3
     if copies > 1:
         addcopy = (copies-1)*0.75
+        price_add = price*(copies-1)
     else:
         addcopy = 0
-    return price + addcopy
+        price_add = 0
+    return price + addcopy + price_add + 3
 
 def main():
     cb_price = 24.95
     qtd_cp = 60
     price = calc_price(cb_price, qtd_cp)
-    print(price)
+    print(round(price,2))
 
 if __name__ == "__main__":
     main()
